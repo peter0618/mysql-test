494a1c072b5af0d8795708fc89898b5e0291382c
peter0618 <peterchoi0618@gmail.com>
2020-06-07 23:05:07 +0900

    first commit

---
+++ src/main/java/com/peter/mysqldemo/MysqlRunner.java
@@ -0,0 +1,38 @@
+package com.peter.mysqldemo;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class MysqlRunner implements ApplicationRunner {
+
+    final DataSource dataSource;
+
+    final JdbcTemplate jdbcTemplate;
+
+    public MysqlRunner(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()){
+            System.out.println(dataSource.getClass());
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE USER (ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id))";
+            statement.executeUpdate(sql);
+        }
+
+        jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'peter')");
+    }
+}
